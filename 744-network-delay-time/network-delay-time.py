5577ed8b9f4318f02ea2704a8680e728bfd2646e
Devansh Alok <35308792+devanshalok@users.noreply.github.com>
2024-09-22 19:32:31 -0700

    Time: 366 ms (76.22%) | Memory: 18.6 MB (78.09%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,39 +1,28 @@
-import heapq
-from typing import List
-
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        # Use a regular dictionary to store edges
-        edges = {}
-        for u, v, w in times:
+        edges=defaultdict(list)
+        for u,v,w in times:
             if u not in edges:
-                edges[u] = []  # Initialize the list if the key doesn't exist
-            edges[u].append((v, w))
+                edges[u]=[]
+            edges[u].append((v,w))
+            
 
-        # Min-heap to store (weight, node)
-        minHeap = [(0, k)]
-        # Set to keep track of visited nodes
-        visit = set()
-        # Variable to store the maximum time
-        result = 0
+        minHeap=[(0,k)]
+        heapq.heapify(minHeap)
+        visit=set()
+        result=0
 
         while minHeap:
-            # Pop the smallest element from the heap
-            w1, n1 = heapq.heappop(minHeap)
-            # Skip if the node is already visited
+            w1,n1 =heapq.heappop(minHeap)
             if n1 in visit:
                 continue
-
-            # Mark the node as visited
+            
             visit.add(n1)
-            # Update the result with the maximum weight
-            result = max(result, w1)
+            result=max(result,w1)
 
-            # Explore the neighbors of the current node
             if n1 in edges:
                 for n2, w2 in edges[n1]:
                     if n2 not in visit:
                         heapq.heappush(minHeap, (w1 + w2, n2))
-
-        # Check if all nodes are visited
-        return result if len(visit) == n else -1
+        
+        return result if len(visit)==n else -1        
