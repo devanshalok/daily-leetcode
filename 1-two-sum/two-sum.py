73111b170dd2415fbcee097044099d9bab538535
Devansh Alok <35308792+devanshalok@users.noreply.github.com>
2024-10-12 00:59:28 -0700

    Time: 44 ms (66.33%) | Memory: 12.5 MB (68.74%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,13 +1,13 @@
 class Solution(object):
     def twoSum(self, nums, target):
+        #dict stores the {number:index} key-value pair
         temp={}
 
-        for i,num in enumerate(nums):
-            k=target-num
-
+        for i,num in enumerate(nums): #enumerate keeps track of the index while iterating
+            k=target-num #temp variable updates with the diff of target and number at current index
             if k in temp:
+                #if k in temp dict return the value of key and the i
                 return [temp[k],i]
-            
             else:
+                #else add the pair to the dict
                 temp[num]=i
-        
