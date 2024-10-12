5fd22b87461b618f53b27f5a73f5a63b87be492b
Devansh Alok <35308792+devanshalok@users.noreply.github.com>
2024-10-12 02:02:45 -0700

    Time: 84 ms (80.15%) | Memory: 21.3 MB (34.17%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,12 +1,15 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        c = Counter(nums)
+        c = Counter(nums) #Counter in built lib creates the freq counter of all elements
+        # Create a heap of most freq occureed elements 
+        #(heap is min heap by default) that's why the -ve value for freq
         c=[(-v,k) for k,v in c.items()]
         heapq.heapify(c)
 
         output=[]
 
         for i in range(k):
+            # Just pop the top k nodes in the heap and append the value to output
             item=heapq.heappop(c)
             output.append(item[1])
 
