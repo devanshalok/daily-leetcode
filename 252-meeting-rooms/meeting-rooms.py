6ce8a154d8416ccaa796aa0e3386de7e65a6b651
Devansh Alok <35308792+devanshalok@users.noreply.github.com>
2024-10-21 22:30:11 -0700

    Time: 4 ms (97.01%) | Memory: 18.9 MB (96.38%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.py
+++ 252-meeting-rooms/meeting-rooms.py
@@ -1,8 +1,12 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        # sort the intervals to get the first time of intervals
         intervals.sort()
 
+        # loop through the intervals 
         for i in range(len(intervals)-1):
+            # if the start of the interval is greater than start of next interval, it means the
+            # meeting times and overlap and we can return False, otherwise True
             if intervals[i][1]>intervals[i+1][0]:
                 return False
         return True
